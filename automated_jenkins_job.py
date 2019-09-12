44a66e4d4f2a884488ab467371fe22790a84bce1
jaronmanyama <42523191+jaronmanyama@users.noreply.github.com>
2019-09-12 15:40:31 -0700

    Automated Jenkins Job

---
+++ automated_jenkins_job.py
@@ -0,0 +1,9 @@
+GameScore1 = int(input('Enter your 1st bowling game score: '))
+GameScore2 = int(input('Enter your 2nd bowling game score: '))
+GameScore3 = int(input('Enter your 3rd bowling game score: '))
+
+TotalGameScore = GameScore1 + GameScore2 + GameScore3
+
+
+
+print('Your total score is:',TotalGameScore)
